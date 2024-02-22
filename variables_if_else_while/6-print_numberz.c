cd575b9804408886dbb3c411182b87c89d74ebd0
Lio-hyper <joshamr11@gmail.com>
2024-02-22 13:41:20 -0400

    Numeros, pero con Z

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int NUM = '0';
+
+	while (NUM <= '9')
+	{
+		putchar(NUM);
+		NUM++;
+	}
+	putchar ('\n');
+	return (0);
+}
