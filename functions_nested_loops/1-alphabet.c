3db89c0e93fd5cfd2dc5ac8bf9c17cd24c641d38
Lio-hyper <joshamr11@gmail.com>
2024-02-23 14:34:29 -0400

    Print the thing

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_alphabet - Check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char ABC = 'a';
+
+	while (ABC <= 'z')
+	{
+		putchar(ABC);
+		ABC++;
+	}
+	putchar ('\n');
+}
